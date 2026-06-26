cd67316334b2ddf556d78669b8c41f204eddaed7
Edward Denis <edorward.21@gmail.com>
2026-06-26 19:29:01 +0300

    Big turn over

---
+++ polls/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    quo_txt = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.quo_txt
+    
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_txt = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_txt
